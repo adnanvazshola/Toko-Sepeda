62eb891215c4e79e5d5b8b12c27791d1582cfa12
adnanvazshola <alvi.4120@students.amikom.ac.id>
2020-06-18 16:08:00 +0700

    CRUD AJAX

--- app/Http/Controllers/merkController.php
+++ app/Http/Controllers/merkController.php
@@ -4,18 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Merk;
+use DataTables;
 
 class merkController extends Controller
 {
-    public function index()
+    public function index(Request $request)
+    /*
     {
-    	$merks = Merk::orderBy('nama')->paginate(5);
 
+    }
+    */
+
+    {
+        if ($request->ajax()) {
+
+            $data = Merk::latest()->get();
+            return DataTables::of($data)
+                    ->addIndexColumn()
+                    ->addColumn('action', function($row){
+                           $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Edit" class="edit btn btn-primary btn-sm editMerk">Edit</a>';
+                           $btn = $btn.' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteMerk">Delete</a>';
+                            return $btn;
+                    })
+                    ->rawColumns(['action'])
+                    ->make(true);
+        }
+        //return view('merk.index');
+    	$merks = Merk::orderBy('nama')->paginate(5);
         return view('merk.index', compact('merks'));
     }
 
     public function store(Request $request)
 	{
+        /*
     	$this->validate($request, [
         	'nama' 		=> 'required|string|max:50|unique:merks',
         	'sales'		=> 'required|string|max:50',
@@ -26,16 +47,33 @@ class merkController extends Controller
     	Merk::create($request->except('_token'));
 
     	return redirect(route('merk.index'))->with(['success' => 'merk berhasil di tambah']);
+        */
+
+        Merk::updateOrCreate(['id' => $request->merk_id],
+                [
+                    'nama'      => $request->nama, 
+                    'slug'      => $request->nama,
+                    'sales'     => $request->sales,
+                    'telephone' => $request->telephone,
+                    'email'     => $request->email,
+                ]);        
+        return response()->json(['success'=>'Merk tersimpan.']);
 	}
 
     public function edit($id)
     {
+        /*
         $merk = Merk::find($id);
         $merks = Merk::orderBy('nama')->paginate(5);
       
         return view('merk.edit', compact('merk','merks'));
-    }
+        */
 
+        $merk = Merk::find($id);
+
+        return response()->json($merk);
+    }
+    /*
     public function update(Request $request, $id)
     {
         $this->validate($request, [
@@ -55,14 +93,19 @@ class merkController extends Controller
       
         return redirect(route('merk.index'))->with(['success' => 'merk telah di update']);
     }
-
+    */
     public function destroy($id)
     {
+        /*
         $merk = Merk::withCount(['child'])->find($id);
         if ($merk->child_count == 0) {
             $merk->delete();
             return redirect(route('merk.index'))->with(['success' => 'merk telah di hapus']);
         }
         return redirect(route('merk.index'))->with(['error' => 'masih ada stok produk pada merk ini']);
+        */
+        Merk::find($id)->delete();
+
+        return response()->json(['success'=>'Merk deleted successfully.']);
     }
 }
--- resources/views/merk/index.blade.php
+++ resources/views/merk/index.blade.php
@@ -4,101 +4,171 @@
     <title>Daftar Merk</title>
 @endsection
 
+@section('head')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+@endsection
+
 @section('content')
 <main class="main">
     <ol class="breadcrumb">
         <li class="breadcrumb-item">Home</li>
         <li class="breadcrumb-item active">merk</li>
     </ol>
-    <div class="container-fluid">
+    <div class="container-fluid container">
         <div class="animated fadeIn">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Tambah Merk</h3>
-                        </div>
-                        <div class="card-body">
-                            <form action="{{ route('merk.store') }}" method="post">
-                            @csrf
-                                <div class="form-group">
-                                    <input type="text" name="nama" class="form-control" placeholder="Masukan nama merk" value="{{ old('nama') }}" required>
-                                    <p class="text-danger">{{ $errors->first('nama') }}</p>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" name="sales" class="form-control" placeholder="Masukan nama lengkap sales" value="{{ old('sales') }}" required>
-                                    <p class="text-danger">{{ $errors->first('sales') }}</p>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" name="telephone" class="form-control" placeholder="Masukan nomer HP" value="{{ old('telephone') }}" required>
-                                    <p class="text-danger">{{ $errors->first('telephone') }}</p>
-                                </div>
-                                <div class="form-group">
-                                    <input type="email" name="email" class="form-control" placeholder="Masukan email sales" value="{{ old('email') }}" required>
-                                    <p class="text-danger">{{ $errors->first('email') }}</p>
-                                </div>
-                                <div class="form-group">
-                                    <button class="btn btn-primary">Tambah</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
+            <a class="btn btn-success" href="javascript:void(0)" id="createNewMerk"> Create New Merk</a>
+            <table class="table table-bordered data-table" id="merkTable">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Merk</th>
+                        <th>Sales</th>
+                        <th>Telephone</th>
+                        <th>email</th>
+                        <th width="280px">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="modal fade" id="ajaxModel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="modelHeading"></h4>
                 </div>
-                <div class="col-md-8">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Daftar Merk</h3>
-                        </div>
-                        <div class="card-body">
-                            @if (session('success'))
-                                <div class="alert alert-success">{{ session('success') }}</div>
-                            @endif
-                            @if (session('error'))
-                                <div class="alert alert-danger">{{ session('error') }}</div>
-                            @endif
-                            <div class="table-responsive">
-                                <table class="table table-hover table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>merk</th>
-                                            <th>Sales</th>
-                                            <th>Telephone</th>
-                                            <th>email</th>
-                                            <th>Aksi</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @forelse ($merks as $m)
-                                            <tr>
-                                                <td></td>
-                                                <td><strong>{{ $m->nama }}</strong></td>
-                                                <td>{{ $m->sales }}</td>
-                                                <td>{{ $m->telephone }}</td>
-                                                <td>{{ $m->email }}</td>
-                                                <td>
-                                                    <form action="{{ route('merk.destroy', $m->id) }}" method="post">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                        <a href="{{ route('merk.edit', $m->id) }}" class="btn btn-warning btn-sm">Edit</a>
-                                                        <button class="btn btn-danger btn-sm">Hapus</button>
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                            @empty
-                                            <tr>
-                                                <td colspan="6" class="text-center">Tidak ada data</td>
-                                            </tr>
-                                        @endforelse
-                                    </tbody>
-                                </table>
+                <div class="modal-body">
+                    <form id="merkForm" name="merkForm" class="form-horizontal">
+                        <input type="hidden" name="merk_id" id="merk_id">
+                        <div class="form-group">
+                            <label for="name" class="col-sm-2 control-label">Nama Merk</label>
+                            <div class="col-sm-12">
+                                <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukan nama merk" value="" maxlength="100" required>
                             </div>
-                            {!! $merks->links() !!}
                         </div>
-                    </div>
+                        <div class="form-group">
+                            <label for="name" class="col-sm-2 control-label">Nama Sales</label>
+                            <div class="col-sm-12">
+                                <input type="text" class="form-control" id="sales" name="sales" placeholder="Masukan nama sales" value="" maxlength="100" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="name" class="col-sm-2 control-label">Telephone</label>
+                            <div class="col-sm-12">
+                                <input type="text" class="form-control" id="telephone" name="telephone" placeholder="Masukan nomor telephone" value="" maxlength="13" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="name" class="col-sm-2 control-label">E-mail</label>
+                            <div class="col-sm-12">
+                                <input type="text" class="form-control" id="email" name="email" placeholder="Masukan email" value="" maxlength="100" required>
+                            </div>
+                        </div>
+                        <div class="col-sm-offset-2 col-sm-10">
+                            <button type="submit" class="btn btn-primary" id="saveBtn" value="create">
+                                Simpan
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 </main>
+@endsection
+
+@section('js')
+
+    <script type="text/javascript">
+        
+        $(function () {
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            var table = $('#merkTable').DataTable({
+              processing: true,
+              serverSide: true,
+              ajax: "{{ route('merk.index') }}",
+              data : { 
+                "id"            : $("#id").val(), 
+                "nama"          : $("#nama").val(), 
+                "sales"         : $("#sales").val(), 
+                "telephone"     : $("#telephone").val(),
+                "email"         : $("#email").val()} ,                
+              columns: [
+                  {data: 'DT_RowIndex', name: 'DT_RowIndex'},
+                  {data: 'nama', name: 'nama'},
+                  {data: 'sales', name: 'sales'},
+                  {data: 'telephone', name: 'telephone'},
+                  {data: 'email', name: 'email'},
+                  {data: 'action', name: 'action', orderable: false, searchable: false},
+              ]
+          });   
+
+
+            $('#createNewMerk').click(function () {
+                $('#saveBtn').val("create-merk");
+                $('#merk_id').val('');
+                $('#merkForm').trigger("reset");
+                $('#modelHeading').html("Tambah Merk");
+                $('#ajaxModel').modal('show');
+            });
+
+            $('body').on('click', '.editMerk', function () {
+              var merk_id = $(this).data('id');
+              $.get("{{ route('merk.index') }}" +'/' + merk_id +'/edit', function (data) {
+                  $('#modelHeading').html("Edit Merk");
+                  $('#saveBtn').val("edit-user");
+                  $('#ajaxModel').modal('show');
+                  $('#merk_id').val(data.id);
+                  $('#nama').val(data.nama);
+                  $('#sales').val(data.sales);
+                  $('#telephone').val(data.telephone);
+                  $('#email').val(data.email);
+              })
+            });
+            
+            $('#saveBtn').click(function (e) {
+                e.preventDefault();
+                $(this).html('Sending..');
+                $.ajax({
+                  data: $('#merkForm').serialize(),
+                  url: "{{ route('merk.store') }}",
+                  type: "POST",
+                  dataType: 'json',
+                  success: function (data) {
+                      $('#merkForm').trigger("reset");
+                      $('#ajaxModel').modal('hide');
+                      table.draw();
+                  },
+                  error: function (data) {
+                      console.log('Error:', data);
+                      $('#saveBtn').html('Save Changes');
+                  }
+              });
+            });
+
+            $('body').on('click', '.deleteMerk', function () {
+                var merk_id = $(this).data("id");
+                confirm("Are You sure want to delete !");
+                $.ajax({
+                    type: "DELETE",
+                    url: "{{ route('merk.store') }}"+'/'+merk_id,
+                    success: function (data) {
+                        table.draw();
+                    },
+                    error: function (data) {
+                        console.log('Error:', data);
+                    }
+                });
+            });
+        }); 
+        
+    </script>
 @endsection
